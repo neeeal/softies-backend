e74aace2bd2a0c8b39ade94f5eb71119e755624e
neeeal <matira.nealbartonjames@gmail.com>
2023-10-27 16:10:24 +0800

    added port and cmd execution condition

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=["GET"])
 def hello_world():
     return 'This is my first API call!'
 
 @app.route('/post', methods=["POST"])
-def testpost():
-     input_json = request.get_json(force=True) 
-     dictToReturn = {'text':input_json['text']}
-     return jsonify(dictToReturn)
+def home():
+    data = request.files['file']
+    return jsonify({"status":"ok"})
+
+if __name__ == '__main__':
+     app.run(port:=8000)
