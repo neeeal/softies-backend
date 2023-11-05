802f39bc476165db63268e3780f4766af7234f81
neeeal <matira.nealbartonjames@gmail.com>
2023-11-05 16:42:53 +0800

    working recommendation route. adds to history for each transaction

--- recommendation.py
+++ recommendation.py
@@ -1,26 +1,23 @@
-from tensorflow.keras.saving import load_model
-import cv2
-import numpy as np
-
-## Testing impors for model
+# from tensorflow.keras.models import load_model
+# import tensorflow_hub as hub
+from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.efficientnet_v2 import EfficientNetV2L
 from tensorflow.keras.layers import Dense, Flatten, Dropout
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras import Sequential
 
 from flask import Flask, render_template, redirect, request, jsonify, session
-# from flask_session import Session
 from pymysql import connect
 from pymysql.cursors import re, DictCursor
-import hashlib
+import numpy as np
+import cv2
+from PIL import Image
 
 app = Flask(__name__)
 # Change this to your secret key (it can be anything, it's for extra protection)
 app.secret_key = 'skanin is the best'
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
-# session = Session(app)
-# print(session)
 
 image_size = (224)
 channels = 3
@@ -33,46 +30,79 @@ connection = connect(host='localhost',
                             #  charset='utf8mb4',
                              cursorclass=DictCursor)
 
-def initModel():
-    ## Model initialization function
-    # IMG_SHAPE = (image_size,image_size) + (channels,) ## RGB=3
-    tf_model = load_model('assets//efficientnetv2-s-imagenet.h5')
-    return tf_model
+## Model initialization
+# Define the function to handle the KerasLayer when loading the model
+# def load_model_with_hub(path):
+#     class KerasLayerWrapper(hub.KerasLayer):
+#         def __init__(self, handle, **kwargs):
+#             super().__init__(handle, **kwargs)
+
+#     custom_objects = {'KerasLayer': KerasLayerWrapper}
+
+#     return load_model(path, custom_objects=custom_objects)
+
+# Load the model using the defined function
+# model = load_model_with_hub('assets//rice_crop_stress_classifier_effnetv2_large.h5')
+# print(model)
+# def initModel():
+#     ## Model initialization function
+#     # IMG_SHAPE = (image_size,image_size) + (channels,) ## RGB=3
+#     tf_model = load_model('assets\rice_crop_stress_classifier_effnetv2_large.h5')
+#     return tf_model
 
 def preprocessData(data):
     ## Main Preprocessing function for input images 
-    processed_data = cv2.resize(data.file,(image_size,image_size),cv2.INTER_LINEAR)
-    processed_data = cv2.cvtColor(processed_data, cv2.COLOR_BGR2RGB)
+    processed_data = cv2.resize(data,(image_size,image_size),cv2.INTER_LINEAR)
     return processed_data
 
 @app.route('/skan', methods=["POST"])
 def skan():
-    ## Prediction route accepting images and outputs prediction of A.I.
-    data = request.files['file']
-    processed_data = preprocessData(data)
-    result = np.argmax(model.predict(processed_data))
-    return jsonify({"result":result})
+    if request.method == 'POST' and 'image' in request.files:
+        ## Retrieving user_id
+        user_id = session.get('user_id')
+        ## Prediction route accepting images and outputs prediction of A.I.
+        ## Read Image from input and convert to CV2
+        image = request.files['image']
+        image_name=image.filename
+        pil_image = Image.open(image.stream).convert('RGB') 
+        data = np.array(pil_image)
+        data = preprocessData(data)
 
-@app.route('/recommendation', methods=["GET"])
-def recommendation():
-    ## Recommendation route based on result
-    stress_id = int(request.args["result"])
-    with connection.cursor() as cursor:
-        cursor.execute(f"SELECT * from rice_stress WHERE `stress_id` = {stress_id}")
-        stress = cursor.fetchone()
-    stress_name = stress['stress_name']
-    stress_type = stress['stress_type']
-    stress_level = stress['stress_level']
-    description = stress['description']
-    description_src = stress['description_src']
-    recommendation = stress['recommendation']
-    recommendation_src = stress['recommendation_src']
-    msg = 'Successfully retrieved recommendation'
-    return jsonify({'msg':msg, 'stress_name':stress_name, stress_type:'stress_type',
-                    'stress_level':stress_level, 'description':description, 'description_src':description_src,
-                    'recommendations':recommendation, 'recommendation_src':recommendation_src})
+        ## Image for saving
+        rice_image = data.tobytes()
+
+        ## Model prediction
+        print("INSERT HERE")
+        result = '3'
+            ## End of prediction
+
+        ## Getting Recommendation using output from model
+        stress_id = int(result)
+        with connection.cursor() as cursor:
+            cursor.execute(f"SELECT * from rice_stress WHERE `stress_id` = {stress_id}")
+            stress = cursor.fetchone()
+        stress_name = stress['stress_name']
+        stress_type = stress['stress_type']
+        stress_level = stress['stress_level']
+        description = stress['description']
+        description_src = stress['description_src']
+        recommendation = stress['recommendation']
+        recommendation_src = stress['recommendation_src']
+        msg = 'Successfully retrieved recommendation'
+
+        ## Creating history entry for transaction
+        with connection.cursor() as cursor:
+            # print("working")
+            sql = "INSERT INTO `history` (`user_id`, `stress_id`, `rice_image`, `image_name`) VALUES (%s, %s, %s, %s)"
+            cursor.execute(sql, (user_id, stress_id, rice_image, image_name))
+            connection.commit()
+        
+        return jsonify({'msg':msg, 'stress_name':stress_name, stress_type:'stress_type',
+                        'stress_level':stress_level, 'description':description, 'description_src':description_src,
+                        'recommendations':recommendation, 'recommendation_src':recommendation_src})
+    msg = 'Invalid request'
+    return jsonify({'msg':msg})
+
 
 if __name__ == '__main__':
-    model = initModel()
-    print('running')
     app.run(port=5000)
