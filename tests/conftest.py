6161cc8c4b47a1316e484547f261ad6d4d6941d1
neeeal <matira.nealbartonjames@gmail.com>
2023-11-28 12:38:59 +0800

    used railway documentation for flask deployment. also updated testing using pytest.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,8 @@
-# import pytest
-# from users import app as flask_app
-# @pytest.fixture
-# def app():
-#     yield flask_app
-# @pytest.fixture
-# def client(app):
-#     return app.test_client()
+import pytest
+from main import app as flask_app
+@pytest.fixture
+def app():
+    yield flask_app
+@pytest.fixture
+def client(app):
+    return app.test_client()
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,10 +1,6 @@
 import json
-import requests
-
-def test_index():
-    ## Testing the index of users api
-    res = requests.get('http://localhost:8080/')
+def test_index(app, client):
+    res = client.get('/')
     assert res.status_code == 200
     expected = {'status': 200}
-    assert expected == json.loads(res._content)
-
+    assert expected == json.loads(res.get_data(as_text=True))
